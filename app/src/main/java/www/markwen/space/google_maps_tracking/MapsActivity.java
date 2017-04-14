f0d46a8addc06e1b33a235f02155a7ffb0bd3c30
Mark Wen <markwenguojie94@gmail.com>
2017-04-13 21:30:42 -0400

    Set up views

--- app/src/main/java/www/markwen/space/google_maps_tracking/MapsActivity.java
+++ app/src/main/java/www/markwen/space/google_maps_tracking/MapsActivity.java
@@ -1,8 +1,19 @@
 package www.markwen.space.google_maps_tracking;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,15 +24,32 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private static final int LOCATIONS_GRANTED = 1;
+    Button recordBtn, stopBtn;
+    LinearLayout btnWrapper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        // Check permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATIONS_GRANTED);
+        }
+
+        // Initialize Buttons
+        initializeButtons();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        setLayoutDimentions(mapFragment, btnWrapper); // Set elements' heights based on screen sizes
+
     }
 
 
@@ -37,10 +65,80 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        updateMapUI(true);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case LOCATIONS_GRANTED: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    updateMapUI(true);
+                }
+                break;
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateMapUI(true);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        updateMapUI(false);
+    }
+
+    private void initializeButtons() {
+        btnWrapper = (LinearLayout)findViewById(R.id.buttonWrapper);
+        recordBtn = (Button)findViewById(R.id.recordButton);
+        stopBtn = (Button)findViewById(R.id.stopButton);
+        stopBtn.setVisibility(View.GONE);
+        recordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recordBtn.setVisibility(View.GONE);
+                stopBtn.setVisibility(View.VISIBLE);
+
+            }
+        });
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recordBtn.setVisibility(View.VISIBLE);
+                stopBtn.setVisibility(View.GONE);
+
+            }
+        });
+    }
+
+    private void setLayoutDimentions(SupportMapFragment mapFragment, LinearLayout btnLayout) {
+        // Get device display dimentions
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        // Set Map height to 3/4 of the display
+        ViewGroup.LayoutParams mapParams = mapFragment.getView().getLayoutParams();
+        // Removing heights of notification bar and bottom navigation
+        mapParams.height = displayMetrics.heightPixels * 3 / 4 - 70;
+
+        // Set Button Wrapper height to 1/4 of the display
+        ViewGroup.LayoutParams wrapperParams = btnLayout.getLayoutParams();
+        wrapperParams.height = displayMetrics.heightPixels / 4;
+    }
+
+    private void updateMapUI(boolean isLocationEnabled) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(isLocationEnabled);
+            mMap.getUiSettings().setCompassEnabled(isLocationEnabled);
+            mMap.getUiSettings().setMyLocationButtonEnabled(isLocationEnabled);
+        }
     }
 }
